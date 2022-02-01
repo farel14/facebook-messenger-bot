75db321b548aafd9acb39b2ecc21d10aed987ad8
farel14 <72905415+farel14@users.noreply.github.com>
2022-02-01 15:02:15 +0700

    change default port to 3000

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()).use(bodyParser.urlencoded({extended:true})); // creates express http server
+
+// Implement express routing
+app.use('/', router)
   
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
